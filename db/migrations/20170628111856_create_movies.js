964a54dc37bdcc80c725f0b431f50fb80706961a
Vinay Padmanabhi <vinay@Vinays-MacBook-Pro.local>
2017-06-29 15:07:32 -0700

    feat(sf_movies_2): POST /movies

---
+++ db/migrations/20170628111856_create_movies.js
@@ -0,0 +1,12 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.createTable('movies', (table) => {
+    table.increments('id').primary();
+    table.text('title').notNullable();
+    table.integer('release_year');
+  });  
+};
+
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('movies'); 
+};
