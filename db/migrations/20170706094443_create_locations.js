37d47020c2cf9eee76f60c207bac12be4644e4d6
Vinay Padmanabhi <vinay@Vinays-MacBook-Pro.local>
2017-07-07 23:43:13 -0700

    feat(sf_movies_4): Fix location migration

--- db/migrations/20170706094443_create_locations.js
+++ db/migrations/20170706094443_create_locations.js
@@ -8,5 +8,5 @@ exports.up = function(knex, Promise) {
 };
 
 exports.down = function(knex, Promise) {
-  return knex.schema.dropTable('locations_movies');
+  return knex.schema.dropTable('locations');
 };
