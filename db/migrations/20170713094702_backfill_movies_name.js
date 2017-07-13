5ba2946057a34d76853da25a57ba55c9c3f038a1
Vinay Padmanabhi <vinay@Vinays-MacBook-Pro.local>
2017-07-13 14:10:48 -0700

    feat(sf_movies_6): Address Tony's comment

--- db/migrations/20170713094702_backfill_movies_name.js
+++ db/migrations/20170713094702_backfill_movies_name.js
@@ -1,7 +1,7 @@
 'use strict';
 
 exports.up = function (Knex, Promise) {
-  return Knex.raw('UPDATE movies SET name = title');
+  return Knex.raw('UPDATE movies SET name = title where title IS NOT NULL');
 };
 
 exports.down = function (Knex, Promise) {
